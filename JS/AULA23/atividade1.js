424167df5ceb50d30771500993c8b2c7eddef70d
techcomkathia <techcomk@gmail.com>
2025-02-19 21:26:19 -0300

    atividade 2 - formulário com manipulação do DOM

--- JS/AULA23/atividade1.js
+++ JS/AULA23/atividade1.js
@@ -17,7 +17,7 @@ let telefone = document.querySelector('#telefone')
 function validarCampos() {
     //nome E email E telefone
     // para o E lógico todas as condições devem ser verdadeiras para retornar um verdadeiro
-    if((nome.value).legth>0 && (email.value).legth>0 && (telefone.value).legth==14){ 
+    if((nome.value).length>0 && (email.value).length>0 && (telefone.value).length>0){ 
         console.log('todos os campos foram preenchidos')
         return true
     } else{
@@ -26,11 +26,39 @@ function validarCampos() {
     }
 }
 
-
 // cadastrarUsuario - a função cadastrarUsuario irá adicionar um novo usuário na lista caso todos os campos sejam preenchidos
+
+function cadastrarUsuario() {
+    if(validarCampos()){
+        let novoUsuario = {
+            nome: nome.value,
+            email: email.value,
+            telefone: telefone.value
+        }
+        usuarios.push(novoUsuario)
+        alert('Usuário cadastrado com sucesso')
+
+        // limpar os campos do formulário
+        nome.value = ''
+        email.value = ''
+        telefone.value = ''
+    }
+    else{
+        alert('Todos os campos devem ser preenchidos')
+    }
+
+}
 //-------------------------------------------------------
 // 3º Passo - remover o evento pre definido do formulario
+formulario.addEventListener('submit', (evento)=>{
+    evento.preventDefault()
+    console.log('Formulario enviado')
+    console.log(usuarios)
+})
+
 //-------------------------------------------------------
+
 // 4º Passo - invocar as funções no botão atráves do evento click
 
-
+btnEnviar.addEventListener('click', cadastrarUsuario)
+// hospedar site no GitHub Pages :)
