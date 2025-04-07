1d939f6144ab6391fe3936d4a97d8bedffd74aaf
techcomkathia <techcomk@gmail.com>
2025-04-07 20:05:22 -0300

    correção atividade1

--- REACT/aula33/src/App.jsx
+++ REACT/aula33/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter} from 'react-router-dom'
 import Rotas from './rotas/Rotas'
-import { BrowserRouter } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
-    
       <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
+      <Rotas/>
+      </BrowserRouter>  
+
     </>
   )
 }
--- REACT/aula33/src/rotas/Rotas.jsx
+++ REACT/aula33/src/rotas/Rotas.jsx
@@ -1,15 +1,26 @@
 import {Routes, Route} from 'react-router-dom'
 import Pagina1 from '../paginas/Pagina1'
 import Pagina2 from '../paginas/Pagina2'
+import Home from '../paginas/Home'
+import Produtos from '../paginas/Produtos'
 import PaginaNaoEncontrada from '../paginas/PaginaNaoEncontrada'
 
 function Rotas() {
- return(
+//  return(
+//     <Routes>
+//         <Route path="/" element={<Pagina1 />} />
+//         <Route path="/sobre" element={<Pagina2 />} />
+//         <Route path="*" element={<PaginaNaoEncontrada />} />
+//     </Routes>)
+
+return(
     <Routes>
-        <Route path="/" element={<Pagina1 />} />
-        <Route path="/sobre" element={<Pagina2 />} />
-        <Route path="*" element={<PaginaNaoEncontrada />} />
-    </Routes>)
+        <Route path='/' element={<Home/>}/>
+        <Route path='/produtos' element={<Produtos/>}/>
+        <Route path='*' element={<PaginaNaoEncontrada/>}/>        
+    </Routes>
+
+)
 
     
 }
