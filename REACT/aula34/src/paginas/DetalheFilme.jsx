bd7ac55af2e07bef583bcfbb2e409c8c9d780a99
techcomkathia <techcomk@gmail.com>
2025-04-09 21:46:38 -0300

    página detalhe de filme, uso de UseParams para parâmetros de rota

--- REACT/aula34/src/paginas/DetalheFilme.jsx
+++ REACT/aula34/src/paginas/DetalheFilme.jsx
@@ -1,7 +1,33 @@
+import filmes from "../assets/filmes"
+import { useParams } from "react-router-dom"
+
+
 function DetalheFilme() {
-    return (
-        <h1>Detalhe do Filme</h1>
-    )
+    const {id} = useParams()
+
+    // com o id recebido, localizar o filme correspondente no array filmes e atribuir a uma variável
+    // mostre o nome do filme no h1
+    // mostre a imagem do filme
+    // nota e ano
+    // descricao
+    const filme = filmes.find((item)=> item.id == id)
+    if(filme){
+    
+        return (
+
+            <>
+            <h1>{filme.nome}</h1>
+            <img src={filme.imagem} />
+            <p>Nota: {filme.nota}</p>
+            <p>Ano: {filme.ano}</p>
+            <p>{filme.descricao}</p>
+            </>
+        )
+           
+    }
+    else{
+        return <h1>Filme não encontrado</h1>
+    }
 }
 
 export default DetalheFilme
