50fd6351c6d05aa43593b3c2d4bbd698bf580a6f
techcomkathia <techcomk@gmail.com>
2025-05-28 20:37:34 -0300

    atividade: trabalhando com rotas e parâmetros de rotas

---
+++ BACK-END/02.NODE/aula03/atividade2.js
@@ -0,0 +1,5 @@
+//crie um servidor que tenha uma rota que retorne o arquivo usuarios.json
+// rota : /usuarios
+// e rotas que retornem cada um dos usuários do arquivo
+// rota : /usuarios/:id
+// id = req.params.id
