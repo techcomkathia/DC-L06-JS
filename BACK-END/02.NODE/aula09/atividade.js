bf2415c8c6a7658d287305a1aa66434e33788231
techcomkathia <techcomk@gmail.com>
2025-06-25 20:01:57 -0300

    arquivos aula 09

---
+++ BACK-END/02.NODE/aula09/atividade.js
@@ -0,0 +1,24 @@
+// instale o bcryptAdd commentMore actions
+//npm install bcrypt
+// use o módulo bcrypt para criptografar a senha do usuario. Você irá receber um objeto com :
+//{
+//     nome: 'Cleitinho',
+//     email: 'cleitinho@exemplo.com',
+//     senha: '123456',
+//     confirmaSenha: '123456'
+// }
+//caso o objeto seja valido, a senha armazenada no objeto deverá ser criptografada e exiba uma mensagem de sucesso no console e armazene o objeto no arquivo usuarios.js
+
+
+
+
+
+
+
+
+
+
+
+//instale o jsonwebtoken para criar um token de autenticação
+//crie uma funcao que recebe um objeto de usuario e caso a senha informada seja equivalente a senha codificada no objeto, exiba um token de autenticação no console. Caso contrário exiba uma mensagem de erro no console
+// exporte o módulo e use-o no arquivo main
